a8deace5f88356454a00840a74afa8e324de3930
Caviles1707 <camilo2012oz@gmail.com>
2025-11-21 19:54:40 -0300

    si

---
+++ T3/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int sugerir_amigos(int id_usuario){ return 0;
+}
+/* ****
+ * int sugerir_amigos
+ ******
+ * Retorna una lista de usuarios que estén exactamente a 2 grados de distancia del usuario dado (amigos de amigos) y que aún no sean amigos directos.
+ ******
+ * Input :
+ * int id_usuario : ID del usuario 
+ ******
+ * Returns :
+ * Lista/Arreglo de enteros, donde cada entero corresponde al ID de un usuario sugerido como posible amigo.
+ **** */
+
+int usuario_mas_popular(){return 0;
+}
+/* ****
+ * int usuario_mas_popular
+ ******
+ * Identifica y retorna el ID del usuario con mayor número de conexiones directas
+ ******
+ * Returns :
+ * int, el ID del usuario más popular.
+ **** */
+
+int contar_comunidades(){return 0;
+}
+/* ****
+ * Funcion    contar_comunidades
+ ******
+ * Resumen
+ * Calcula el número total de comunidades del grafo.
+ **** */
+int calcular_influencia(int id_usuario){return 0;
+}
+int encontrar_puentes(){return 0;
+}
+void menu(){ 
+} // Menu
+int main(){
+  cout << "Hello World" << endl;
+  return 0;
+}
